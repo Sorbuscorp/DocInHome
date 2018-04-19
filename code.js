244656c7ce58b4e92ca81c32c4952d2a71845506
Sorbus <sorbuscorp@gmail.com>
2018-04-19 14:52:21 +0300

    Version 0 Hello World datte bae!))

---
+++ code.js
@@ -0,0 +1,13 @@
+/*Здесь будут какие-то скрипты */
+//этот конкретно кусок вызыват форму регистрации при нажатии на слово зарегистрироваться
+function showLogBox(){
+    document.getElementById("Reg").style.display="block";
+}
+
+onload=function(){
+    var forma= document.getElementById("Reg");
+    forma.style.display="none";
+    document.getElementById("registration").onclick=showLogBox;
+    
+}
+
